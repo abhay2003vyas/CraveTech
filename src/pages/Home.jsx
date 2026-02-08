7708a79683d5839328c89b07f4c13c96ca47e5c4
Abhay Vyas <vyas0994@gmail.com>
2026-02-08 22:54:26 +0530

    reove navbaer and footer from the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,17 @@
 // src/pages/Home.jsx
-import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
 import Stats from "../components/Stats";
 import Features from "../components/Features";
 import CTA from "../components/CTA";
-import Footer from "../components/Footer";
+
 
 export default function Home() {
   return (
     <>
-      <Navbar />
       <Hero />
       <Stats />
       <Features />
       <CTA />
-      <Footer />
     </>
   );
 }
