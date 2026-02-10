a7d5db2f02297ef548e459e3f5b9ae2879993751
Abhay Vyas <vyas0994@gmail.com>
2026-02-11 00:43:57 +0530

    update api

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://cravetechbackend.onrender.com/api",
+     baseURL: "https://cravetechbackend.onrender.com/api",
+  // baseURL: "http://localhost:8080/api",
 });
 
 export default api;
